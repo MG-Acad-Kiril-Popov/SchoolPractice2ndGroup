fe56f20dee86bc35c5677ca0e5ae9fb15ba2d170
hoseuz <yosifgadzhev_zh19@schoolmath.eu>
2023-12-18 08:34:27 +0200

    updated application

--- application.cs
+++ application.cs
@@ -2,20 +2,23 @@ class EducationalProgram
 {
     static void Main()
     {
-        StudentAndTeacherEntity studentAndTeacher1 = new StudentAndTeacherEntity("John", "Doe", new DateTime(1995, 5, 15), "123 Main St", 12345, "Computer Science", 3.75, 2, "Science");
-        StudentAndTeacherEntity studentAndTeacher2 = new StudentAndTeacherEntity("Alice", "Johnson", new DateTime(1998, 10, 8), "456 Elm St", 54321, "Mathematics", 3.90, 5, "Mathematics");
+        TeacherEntity teacher = new StudentEntity("John", "Doe", new DateTime(1995, 5, 15), "123 Main St", 12345, "Computer Science", 3.75, 2, "Science");
+        StudentEntity student = new StudentEntity("Alice", "Johnson", new DateTime(1998, 10, 8), "456 Elm St", 54321, "Mathematics", 3.90, 5, "Mathematics");
         AttendanceStatistics statistics = new AttendanceStatistics();
+     
 
-        studentAndTeacher1.AttendClass();
-        studentAndTeacher2.AttendClass();
-        studentAndTeacher1.TeachLesson();
-        studentAndTeacher2.ConductMeeting();
+        student.AttendClass();
+        teacher.AttendClass();
+        student.AttendClass();
+        teacher.TeachLesson();
+        student.ConductMeeting();
 
-        statistics.RecordAttendance(studentAndTeacher1);
-        statistics.RecordAttendance(studentAndTeacher2);
+        statistics.RecordAttendance(student);
+        statistics.RecordAttendance(teacher);
 
         statistics.DisplayAttendanceStatistics();
         statistics.RankByGPAOrExperience();
     }
 }
+
 
